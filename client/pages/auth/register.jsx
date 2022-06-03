9a74d1daf36baae4181a5c0e626805b05bab721f
NetHunter123 <viktordmitryk3@gmail.com>
2022-06-04 01:33:30 +0300

    save strapi config

--- client/pages/auth/register.jsx
+++ client/pages/auth/register.jsx
@@ -110,7 +110,7 @@ const Register = () => {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              Sign Up
             </Typography>
             <Box
               component="form"
